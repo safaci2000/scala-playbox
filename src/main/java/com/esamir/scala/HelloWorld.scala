55f0b53ee73c9400cdff4a3fb0a5f827852251c2
Samir Faci <samir@esamir.com>
2015-01-25 09:38:21 -0800

    Creating a dummy maven project to build a scala application

---
+++ src/main/java/com/esamir/scala/HelloWorld.scala
@@ -0,0 +1,10 @@
+package com.esamir.scala
+
+/**
+ * Created by user on 1/25/15.
+ */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
---
+++ src/test/java/com/esamir/scala/AppTest.java
@@ -0,0 +1,18 @@
+package com.esamir.scala;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Dummy Test example in java.
+ */
+public class AppTest {
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
