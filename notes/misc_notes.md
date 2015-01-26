c6efeeb892a2f5797098e88ea56d543d2a04b3f5
Samir Faci <samir@esamir.com>
2015-01-25 16:25:31 -0800

    Adding some basic datatype operations.

---
+++ notes/misc_notes.md
@@ -0,0 +1,19 @@
+# Misc Notes
+
+val == read only
+var == mutable
+
+## multiline
+
+var foobar =
+  |   "this is a test"
+
+## Arrays
+
+Size of array can't be changed but the value is.  Hence arrays is a mutable construct.
+
+
+newList = 5 :: oldList // returns a new list created by prepending the new element.
+
+aList ::: bList // returns a concatenation of 2 lists.
+
--- src/main/java/com/esamir/scala/HelloWorld.scala
+++ src/main/java/com/esamir/scala/HelloWorld.scala
@@ -25,18 +25,64 @@ object HelloWorld {
 
 
   // Runner methods
-  def factorialPlay(): Unit = {
+  def factorialPlay(target: Long ): Unit = {
     //java
     var start: Long = System.currentTimeMillis()
-    println("the factorial of 30 via java: " + factorial(BigInteger.valueOf(30L)))
+    val target:Long = 100L  // Interesting.  This overrides method parameter
+    println(s"the factorial of ${target} via java: ${factorial(BigInteger.valueOf(target))}")
     var end: Long = System.currentTimeMillis()
-    println(s"It took ${end - start} milliseconds to calculate fact(30) in java")
+    println(s"It took ${end - start} milliseconds to calculate fact(${target}) in java")
 
     //Scala
     start = System.currentTimeMillis()
-    println("the factorial of 30 via scala: " + factorialScala(BigInteger.valueOf(30L)))
+    println(s"the factorial of ${target} via java: ${factorialScala(BigInteger.valueOf(target))}")
     end = System.currentTimeMillis()
-    println(s"It took ${end - start} milliseconds to calculate fact(30) in scala")
+    println(s"It took ${end - start} milliseconds to calculate fact(${target}) in scala")
+  }
+
+  def dataTypes(): Unit = {
+    //Arrays
+    val greeting = new Array[String](3)
+    val anotherArray = Array("OMG", "Like", "Hi!!!")
+    greeting(0) = "Hello"
+    greeting(1) = "World"
+    greeting(2) = "!!!"
+
+    //Another variation of the same code
+
+    //so pretty
+    greeting.foreach(i=> print(s"${i} "))
+
+    for(i <- 0.to(2)) {
+      print(s"${greeting(i)} ")
+    }
+    print("\n")
+    for(i <- 0 to 2 ) {
+      print(s"${greeting(i)} ")
+    }
+    print("\n")
+    for(i <- 0 to anotherArray.length - 1) {
+      print(anotherArray(i))
+
+    }
+    //List
+    print("\n")
+    val list = List(7, 9, 3)
+    //prepend an element and return a new list.
+    val modifiedList = 1 :: list
+
+    modifiedList.foreach(f=> println(f))
+//    for (i <- 0 to modifiedList.length - 1) {
+//        print(s"${modifiedList(i)} ")
+//    }
+
+    print("\n")
+    val mergedList = list ::: modifiedList
+    print(mergedList)
+    print("\n")
+    print( mergedList.count(p => p == 7  )  )
+
+
   }
 
 
@@ -47,13 +93,11 @@ object HelloWorld {
     capital += ("Japan" -> "Tokyo")
     println(capital)
 
-    factorialPlay()
+//    factorialPlay(50L)
+    dataTypes()
 
     //Class fun time.
     var dummy:Dummy = new Dummy(1, "foobar")
-
-
-
 
   }
 }
